e59238c7df735962c51fcbb0653de82e1d980575
Erij <123583458+AreejSayari@users.noreply.github.com>
2023-12-02 21:34:57 +0100

    Add files via upload

---
+++ src/main/java/tn/enit/tp1/AverageReducer.java
@@ -0,0 +1,26 @@
+package tn.enit.tp1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public static class AverageReducer extends Reducer<Text, NumPair, Text, DoubleWritable> {
+    private DoubleWritable result = new DoubleWritable();
+
+    public void reduce(Text key, Iterable<NumPair> values, Context context) throws IOException, InterruptedException {
+        int sumHours = 0;
+        int count = 0;
+
+        for (NumPair value : values) {
+            sumHours += value.getFirst();
+            count += value.getSecond();
+        }
+
+        if (count > 0) {
+            result.set((double) sumHours / count);
+            context.write(key, result);
+        }
+    }
+}
---
+++ src/main/java/tn/enit/tp1/NumPair.java
@@ -0,0 +1,42 @@
+package tn.enit.tp1;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import java.io.IOException;
+public static class NumPair implements Writable {
+    private int first;
+    private int second;
+
+    public NumPair() {}
+
+    public NumPair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        out.writeInt(first);
+        out.writeInt(second);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        first = in.readInt();
+        second = in.readInt();
+    }
+}
